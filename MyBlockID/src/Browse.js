9155e00c17cc8eb53030d334cf50062cd87bd7d3
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-02-28 18:17:22 +0000

    Init MyBlockID web app project

---
+++ MyBlockID/src/Browse.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Browse extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Browse</h2>
+        <p>Browse your IDs</p>
+      </div>
+    );
+  }
+}
+
+export default Browse;
