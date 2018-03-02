aa14d7e6f13bf233fff9ecefc02359c9fc4cbe05
Adriano Campos <adrianoribeirocampos@gmail.com>
2018-03-02 19:19:11 +0000

    Init dummy content

--- MyBlockID/src/index.js
+++ MyBlockID/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-import "./index.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <Main/>,
